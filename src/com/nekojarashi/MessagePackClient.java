d9b822d469bb71dc34ad7224d78dfd605655da4a
Yuki Okubo <y-okubo@nekojarashi.com>
2015-02-23 17:22:29 +0900

    Update

--- src/com/nekojarashi/MessagePackClient.java
+++ src/com/nekojarashi/MessagePackClient.java
@@ -6,12 +6,16 @@
 package com.nekojarashi;
 
 import static java.lang.System.exit;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.msgpack.rpc.Client;
+import org.msgpack.rpc.Future;
 import org.msgpack.type.Value;
 
 /**
@@ -32,20 +36,82 @@ public class MessagePackClient {
             Logger.getLogger(MessagePackClient.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        Value result = client.callApply("hello", new Object[]{});
+//        Value result = client.callApply("hello", new Object[]{});
+//
+//        System.out.println(result.toString());
+//
+//        for (int i = 0; i < 1000; i++) {
+//            result = client.callApply("hello", new Object[]{});
+//            System.out.println(result.toString());
+//        }
+//
+//        result = client.callApply("hello2", new Object[]{});
+//
+//        System.out.println(result.asArrayValue().toString());
+//
+//        result = client.callApply("hello2", new Object[]{});
+//        List<Value> list = result.asArrayValue();
+//        ArrayList<String> art = new ArrayList(list);
+//        System.out.println(list.get(0).toString());
+//        Future<Value> result;
+//        result = client.callAsyncApply("async_hello", new Object[]{});
+//        
+//        try {
+//            System.out.println(result.get().toString());
+//        } catch (InterruptedException ex) {
+//            Logger.getLogger(MessagePackClient.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        
+//        System.out.println("AAAAA");
+        Value result;
+        String method = "";
 
-        System.out.println(result.toString());
+        result = client.callApply("method_with_argument", new Object[]{new String("ABC")});
+        result = client.callApply("method_with_argument", new Object[]{new Integer(Integer.MAX_VALUE)});
+        result = client.callApply("method_with_argument", new Object[]{new Long(Long.MAX_VALUE)});
+        result = client.callApply("method_with_argument", new Object[]{new Float(Float.MAX_VALUE)});
+        result = client.callApply("method_with_argument", new Object[]{new Double(Double.MAX_VALUE)});
+        result = client.callApply("method_with_argument", new Object[]{new BigDecimal(Integer.MAX_VALUE)});
+        result = client.callApply("method_with_argument", new Object[]{new Boolean(true)});
 
-        result = client.callApply("hello2", new Object[]{});
+        ArrayList array = new ArrayList();
+        array.add("A");
+        array.add("B");
+        array.add(100);
+        result = client.callApply("method_with_argument", new Object[]{array});
 
-        System.out.println(result.asArrayValue().toString());
+        HashMap hash = new HashMap();
+        hash.put("A", "AAA");
+        hash.put("B", "BBB");
+        hash.put("C", 10000);
+        hash.put(200, 20000);
+        result = client.callApply("method_with_argument", new Object[]{hash});
 
-        result = client.callApply("hello2", new Object[]{});
-        List<Value> list = result.asArrayValue();
-        ArrayList<String> art = new ArrayList(list);
-        System.out.println(list.get(0).toString());
+        method = "method_return_fixnum";
+        result = client.callApply(method, new Object[]{});
+        System.out.println(method + "(" + result.getType().toString() + "): " + result.toString());
 
-        exit(0);        
+        method = "method_return_bignum";
+        result = client.callApply(method, new Object[]{});
+        System.out.println(method + "(" + result.getType().toString() + "): " + result.toString());
+
+        method = "method_return_string";
+        result = client.callApply(method, new Object[]{});
+        System.out.println(method + "(" + result.getType().toString() + "): " + result.toString());
+
+        method = "method_return_boolean";
+        result = client.callApply(method, new Object[]{});
+        System.out.println(method + "(" + result.getType().toString() + "): " + result.toString());
+
+        method = "method_return_array";
+        result = client.callApply(method, new Object[]{});
+        System.out.println(method + "(" + result.getType().toString() + "): " + result.toString());
+
+        method = "method_return_hash";
+        result = client.callApply(method, new Object[]{});
+        System.out.println(method + "(" + result.getType().toString() + "): " + result.toString());
+
+        exit(0);
     }
 
 }
